52af530ed2207429942c6f2e5f951cca061f6bcc
Abhishek-Mishra987 <abhishekmishraa987@gmail.com>
2025-12-07 09:34:21 +0000

    operations

---
+++ 01_basics/02_conversionOperator.js
@@ -0,0 +1,25 @@
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ console.log("================");
+ 
+ //**************************Operations***************** */
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // here output will be 122, bcz it will treat every digit as character(since first is character)
+console.log(1 + 2 + "3"); // here output will be 33
+
+
+console.log(+true);
+console.log(+"");
+
+
+
