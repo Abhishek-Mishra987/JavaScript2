5ee544051d87139e362fd2f7859aac88ec3e4534
Abhishek-Mishra987 <abhishekmishraa987@gmail.com>
2025-12-18 08:31:16 +0000

    functions continued..

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,3 +22,48 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage("Abhishek"));
+
+
+//=====================================================================================
+
+// (...)
+//Rest → Pack things together
+// Spread → Open things up
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500))
+
+
+const user = {
+  username: "Abhishek",
+  price: 199
+}
+
+//===============================================================================
+
+// handling objects
+
+function handleObject(anyobject){
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);  
+}
+
+//handleObject(user);
+
+handleObject({
+  username: "Mishra",
+  price: 3456
+});
+
+// we can also handle arrays
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
