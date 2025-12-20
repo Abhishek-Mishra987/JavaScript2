ee7583c6a46e2758b15e4358300e535a0921ab62
Abhishek-Mishra987 <abhishekmishraa987@gmail.com>
2025-12-20 16:53:33 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 6; i++) {
+    const element = i;
+    console.log(element);
+    
+    
+}
+
+
+let num = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(num + " x " + i + " = " + (num * i));
+}
