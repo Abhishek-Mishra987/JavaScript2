ad1cd1ec57283e06b77511a3a1a740e4dead173d
Abhishek-Mishra987 <abhishekmishraa987@gmail.com>
2025-12-20 10:35:42 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+// Control Flow determines the order in which statements are executed in a JavaScript program.
+let age = 14
+if (age >= 18) {
+  console.log("Adult");
+}
+else{
+    console.log("Minor");
+}
+
+//=========================================================================
+
+const bal = 500
+if(bal > 300) console.log("greater than 300");
+
+//============================================================
+
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("Allowed to login");
+}
+
+//======================================================================================
+
